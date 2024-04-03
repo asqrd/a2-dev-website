8f57c0f824a377bfa31b59b79be817ad7784c573
Yusri <yusrimathews@gmail.com>
2024-04-03 21:47:29 +0200

    Updated section link, image attributes and cosmetic changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import { CheckIcon } from '@heroicons/react/20/solid';
 import Image from 'next/image';
-
-const includedFeatures = [
-  'Private forum access',
-  'Member resources',
-  'Entry to annual conference',
-  'Official member t-shirt',
-]
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -17,7 +11,7 @@ export default function Home() {
             <div className="flex lg:flex-1">
               <a href="/" className="shadow-md focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">
                 <span className="sr-only">A Squared &mdash; Development</span>
-                <Image className="h-16 w-auto" src="a2.svg" alt="A Squared - Development" />
+                <Image className="h-16 w-auto" src="a2.svg" alt="A Squared - Development" width={0} height={0} />
               </a>
             </div>
             <div className="lg:flex lg:flex-1 lg:justify-end">
@@ -37,9 +31,9 @@ export default function Home() {
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="text-center">
               <h1 className="text-4xl font-bold text-gray-900 sm:text-6xl">We craft cutting-edge technology solutions from the future.</h1>
-              <p className="mt-6 text-lg leading-8 text-gray-600">Redefining what&apos;s possible, integrating seamlessly and <br />offering a glimpse into tomorrow&apos;s world... today.</p>
+              <p className="mt-6 text-lg leading-8 text-gray-600 px-5">Redefining what&apos;s possible, integrating seamlessly and <br className="hidden md:block" />offering a glimpse into tomorrow&apos;s world... today.</p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a href="#services" className="text-sm font-semibold leading-6 text-gray-900 px-1.5 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Our services <span aria-hidden="true">&rarr;</span></a>
+                <Link href="#services" className="text-sm font-semibold leading-6 text-gray-900 px-1.5 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Our services <span aria-hidden="true">&rarr;</span></Link>
               </div>
             </div>
           </div>
@@ -48,25 +42,25 @@ export default function Home() {
 
       <div className="bg-white border-y border-solid">
         <div className="mx-auto max-w-7xl pt-24 pb-32 px-6 lg:px-8">
-          <p className="text-center text-lg font-semibold text-gray-900">Agencies and startups we work with:</p>
+          <p className="text-center text-lg font-semibold text-gray-900">Agencies and startups <br className="sm:hidden" />we work with:</p>
 
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-sm sm:grid-cols-4 sm:gap-x-10 md:mx-0 md:max-w-none md:grid-cols-4">
-            <Image className="col-span-2 max-h-12 w-full object-contain md:col-span-1" src="doctify.svg" alt="Doctify" />
-            <Image className="col-span-2 max-h-12 w-full object-contain md:col-span-1" src="akqa.svg" alt="AKQA" />
-            <Image className="col-span-2 max-h-12 w-full object-contain md:col-span-1" src="mintago.svg" alt="Mintago" />
-            <Image className="col-span-2 max-h-12 w-full object-contain md:col-span-1" src="perkbox.svg" alt="Perkbox" />
+            <Image className="col-span-2 max-h-12 w-full object-contain md:col-span-1" src="doctify.svg" alt="Doctify" width={0} height={0} />
+            <Image className="col-span-2 max-h-12 w-full object-contain md:col-span-1" src="akqa.svg" alt="AKQA" width={0} height={0} />
+            <Image className="col-span-2 max-h-12 w-full object-contain md:col-span-1" src="mintago.svg" alt="Mintago" width={0} height={0} />
+            <Image className="col-span-2 max-h-12 w-full object-contain md:col-span-1" src="perkbox.svg" alt="Perkbox" width={0} height={0} />
           </div>
         </div>
       </div>
 
-      <div className="bg-white pt-24 pb-36 overflow-hidden">
+      <div id="services" className="bg-white pt-24 pb-36 overflow-hidden">
         <div className="relative isolate mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl text-center mb-20">
             <h2 className="text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl">Here&apos;s what we offer</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">Our team has over a decade of experience building consumer <br />and enterprise technology solutions and experiences.</p>
+            <p className="mt-6 text-lg leading-8 text-gray-600 px-5">Our team has over a decade of experience building consumer <br className="hidden md:block" />and enterprise technology solutions and experiences.</p>
           </div>
 
-          <div className="md:flex mt-10 px-16">
+          <div className="md:flex mt-10 md:px-16">
             <div className="flex-1 p-8 sm:p-10 md:mb-0 mb-10 rounded-3xl ring-1 ring-gray-200 mx-5 bg-white">
               <h3 className="text-2xl font-bold tracking-tight text-gray-900">Full-stack Development</h3>
               <p className="mt-6 text-base leading-7 text-gray-600">Tailor-made solutions designed to fit your unique business needs, offering unmatched efficiency.</p>
@@ -96,7 +90,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="md:flex mt-10 px-16">
+          <div className="md:flex mt-10 md:px-16">
             <div className="flex-1 p-8 sm:p-10 md:mb-0 mb-10 rounded-3xl ring-1 ring-gray-200 mx-5 bg-white">
               <h3 className="text-2xl font-bold tracking-tight text-gray-900">Cloud Services</h3>
               <p className="mt-6 text-base leading-7 text-gray-600">Unlock the power of scalability with our cutting-edge cloud solutions tailored to meet your business needs.</p>
